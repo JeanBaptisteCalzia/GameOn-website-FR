1341526d63dd7d40ed7697db4a2d73289f61b466
JBC <hello@jeanbaptistecalzia.fr>
2024-02-29 15:46:11 +0100

    Add Close modal form function

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,23 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalCloseBtn = document.querySelector(".close");
+const modalContent = document.querySelector(".content");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Close modal event
+modalCloseBtn.addEventListener("click", () => {
+  closeModal();
+});
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// Close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
